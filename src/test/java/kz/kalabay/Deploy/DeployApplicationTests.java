4286ff581530bc9b2445258dcf37185e78928b09
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2024-07-30 15:33:37 +0500

    sad

---
+++ src/test/java/kz/kalabay/Deploy/DeployApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.kalabay.Deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
