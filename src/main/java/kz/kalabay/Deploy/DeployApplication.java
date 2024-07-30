ea5bea7fc425766059b95fb2e6c3334407346be3
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2024-07-30 15:35:51 +0500

    ad

--- src/main/java/kz/kalabay/Deploy/DeployApplication.java
+++ src/main/java/kz/kalabay/Deploy/DeployApplication.java
@@ -2,6 +2,9 @@ package kz.kalabay.Deploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DeployApplication {
@@ -9,5 +12,11 @@ public class DeployApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DeployApplication.class, args);
 	}
-
+}
+@RestController
+class DemoController{
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hello World";
+	}
 }
